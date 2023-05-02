0db50b6a863cb29c6d6e0ad502015a81ebea00e2
RenaldasZ <spikeris2@gmail.com>
2023-05-02 20:04:36 +0300

    update

--- app.py
+++ app.py
@@ -1,39 +1,56 @@
 import PySimpleGUI as sg
 
-# Define the layout of the GUI
+# Define the PySimpleGUI layout
 layout = [
-    [sg.Text('Kelionės atstumas (km): '), sg.InputText(key='atstumas')],
-    [sg.Text('Kuros sanaudos (l/100km): '), sg.InputText(key='sanaudos')],
-    [sg.Text('Kuro bako talpa (l): '), sg.InputText(key='talpa')],
-    [sg.Button('Skaiciuoti')],
-    [sg.Text(size=(40, 1), key='output')]
+    [sg.Text('Kelionės atstumas (km):'), sg.Input(key='distance')],
+    [sg.Text('Kuro bako talpa (l):'), sg.Input(key='tank_size')],
+    [sg.Text('Kuro kaina (eur/l):'), sg.Input(key='fuel_cost')],
+    [sg.Checkbox('Maistas', key='food')],
+    [sg.Checkbox('Kelių mokestis', key='tolls')],
+    [sg.Text('Valiutos tipas: Euro')],
+    [sg.Text('Asmeninės išlaidos:', size=(20, 1)), sg.Input(key='personal_expenses')],
+    [sg.Button('Skaičiuoti'), sg.Button('Išvalyti')],
+    [sg.Text('Kelionės pradžios laikas:'), sg.Input(key='start_time')],
+    [sg.Multiline(key='history', size=(40, 5))],
+    [sg.Text('Kiek laiko truko kiekviena kelionė:'), sg.Multiline(key='duration')],
+    [sg.Text('Kiek laiko truko visos kelionės:'), sg.Input(key='total_duration')],
+    [sg.Text('Bendra kuro sanaudų suma (eur):'), sg.Input(key='total_fuel_cost')],
 ]
 
-# Create the window and display it
+# Create the PySimpleGUI window
 window = sg.Window('Kelionės kalkuliatorius', layout)
 
-# Event loop to process events and get input values
+# Event loop to process events and get input
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == 'Skaiciuoti':
-        try:
-            atstumas = float(values['atstumas'])
-            sanaudos = float(values['sanaudos'])
-            talpa = float(values['talpa'])
-            kuro_kaina = 1.5  # Example fuel price
-            kaina = (atstumas / 100) * sanaudos * kuro_kaina
-            if kaina > 0:
-                liko_km = talpa / sanaudos * 100 - atstumas
-                if liko_km > 0:
-                    output_str = f'Kelionė kainuos {kaina:.2f}€. Liko kuro {liko_km:.1f}km'
-                else:
-                    output_str = f'Kelionė kainuos {kaina:.2f}€. Kuro neužteks iki paskirties vietos.'
-            else:
-                output_str = 'Įveskite teisingus skaičius.'
-        except ValueError:
-            output_str = 'Įveskite teisingus skaičius.'
-        window['output'].update(output_str)
 
+    # Break out of the loop if the user closes the window or clicks the Exit button
+    if event == sg.WINDOW_CLOSED or event == 'Išvalyti':
+        break
+
+    # Calculate the total fuel cost based on the input values
+    if event == 'Skaičiuoti':
+        distance = float(values['distance'])
+        tank_size = float(values['tank_size'])
+        fuel_cost = float(values['fuel_cost'])
+        food = values['food']
+        tolls = values['tolls']
+        personal_expenses = float(values['personal_expenses'])
+
+        total_fuel_cost = (distance / tank_size) * fuel_cost
+
+        if food:
+            total_fuel_cost += 20.0
+        if tolls:
+            total_fuel_cost += 10.0
+
+        total_fuel_cost += personal_expenses
+
+        # Update the output fields
+        window['total_fuel_cost'].update(total_fuel_cost)
+        window['history'].update(values['history'] + f'Kelionė: {distance} km, Kaina: {total_fuel_cost} eur\n')
+        window['duration'].update(values['duration'] + f'{distance} km: 3 val. 30 min.\n')
+        window['total_duration'].update('20 val.')
+        
+# Close the PySimpleGUI window
 window.close()
