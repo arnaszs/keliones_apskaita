eaeb945bb7e0c755065c3772104971ffb33c24be
RenaldasZ <spikeris2@gmail.com>
2023-05-04 21:10:27 +0300

    update

--- funkcijos.py
+++ funkcijos.py
@@ -23,50 +23,46 @@ table = sg.Table(
     expand_x=True,
     expand_y=True)
 
-
-layout = [    [sg.Text('Keliones pavadinimas: ', background_color='Dark grey'), sg.Input(key='-NAME-')],
-    [sg.Text('Keliones atstumas (km): ', background_color='Dark grey'), sg.Input(key='-DISTANCE-', size=20)],
-    [sg.Text('Greitis: ', background_color='Dark grey'), sg.Input(key='-SPEED-')],
-    [sg.Text('Kuro bako talpa (l): ', background_color='Dark grey'), sg.Input(key='-FUEL_CAPACITY-')],
-    [sg.Text('Kuro kaina (eur/l): ', background_color='Dark grey'), sg.Input(key='-FUEL_PRICE-')],
-    [sg.Text('Kuro sanaudos (l/100km): ', background_color='Dark grey'), sg.Input(key='-FUEL_CONSUMPTION-')],
-    [sg.Checkbox('Maistas', key='-FOOD_CHECK-', background_color='Dark grey')],
-    [sg.Checkbox('Keliu mokestis', key='-TOLL_CHECK-', background_color='Dark grey')],
-    [sg.Text('Valiutos tipas: ', background_color='Dark grey'),     sg.Radio('Eurai', 'RADIO1', key='-EURO_RADIO-', default=True, background_color='Dark grey'),     sg.Radio('Svarai', 'RADIO1', key='-POUND_RADIO-', background_color='Dark grey')],
-    [sg.Button('Skaiciuoti', button_color=('white', 'springgreen4'), use_ttk_buttons=True, focus=True),     sg.Button('Isvesti', use_ttk_buttons=True, focus=True),     sg.Button('Rodyti ataskaita', use_ttk_buttons=True, focus=True),     sg.Button('Iseiti', button_color=('white', 'firebrick3'), use_ttk_buttons=True, focus=True)],
+# Layout
+layout = [
+    [sg.Text('Keliones pavadinimas: ', background_color="Dark Cyan"), sg.Input(key='-NAME-')], 
+    [sg.Text('Kelionės atstumas (km): ', background_color="Dark Cyan"), sg.Input(key='-DISTANCE-', size=20)],
+    [sg.Text('Greitis: ', background_color="Dark Cyan"), sg.Input(key='-SPEED-')],
+    [sg.Text('Kuro bako talpa (l): ', background_color="Dark Cyan"), sg.Input(key='-FUEL_CAPACITY-')],
+    [sg.Text('Kuro kaina (eur/l): ', background_color="Dark Cyan"), sg.Input(key='-FUEL_PRICE-')],
+    [sg.Text('Kuro sanaudos (l/100km): ', background_color="Dark Cyan"), sg.Input(key='-FUEL_CONSUMPTION-')],
+    [sg.Checkbox('Maistas', key='-FOOD_CHECK-', background_color="Dark Cyan")],
+    [sg.Checkbox('Kelių mokestis', key='-TOLL_CHECK-', background_color="Dark Cyan")],
+    [sg.Text('Valiutos tipas: ', background_color="Dark Cyan"), sg.Radio('Eurai', 'RADIO1', key='-EURO_RADIO-', default=True, background_color="Dark Cyan"), sg.Radio('Svarai', 'RADIO1', key='-POUND_RADIO-', background_color="Dark Cyan")],
+    [sg.Button('Skaičiuoti', button_color=('white', 'springgreen4'), use_ttk_buttons=True, focus=True), sg.Button('Išvalyti', use_ttk_buttons=True, focus=True), sg.Button('Rodyti ataskaitą', use_ttk_buttons=True, focus=True), sg.Button('Išeiti', button_color=('white', 'firebrick3'), use_ttk_buttons=True, focus=True)],
     [table]
-]
+    ]
 
-layout2 = [    [sg.Text('Andriaus keliones!', background_color='green', text_color='white', justification='c', key='-T-', font=('Bodoni MT', 40))],
-    [sg.Image(key='-IMAGE-')],
-    [sg.ProgressBar(interframe_duration, orientation='h', size=(58, 30), bar_color=('green', 'white'), key='progress', relief=sg.RELIEF_FLAT)],
-    [sg.Button(button_color=('white', 'firebrick3'), button_text=('Atsaukti'), size=(20, 1), focus=True, key='Iseiti')]
-]
+layout2 = [[sg.Text('Andriaus kelionės!', background_color='#A37A3B', text_color='#FFF000',  justification='c', key='-T-', font=("Bodoni MT", 40))],
+        [sg.Image(key='-IMAGE-')],
+        [sg.ProgressBar(interframe_duration, orientation='h', size=(58, 30), key='progress')],
+        [sg.Button(button_color=('white', 'firebrick3'), button_text=('Atšaukti'), size=(20, 1), focus=True, key='Išeiti')]]   
 
 # Create the window
-window = sg.Window('Keliones kalkuliatorius',
-                    layout,
-                    size=(1280, 480),
-                    margins=(None, None),
-                    button_color=None,
-                    font='Italic 12 bold',
-                    background_color='Dark grey',
-                    icon=icon,
-                    alpha_channel=0.97,
-                    use_default_focus=True,
-                    grab_anywhere=True,
-                    resizable=True,
-                    element_justification='left',
-                    titlebar_font='Italic 12 bold',
-                    titlebar_icon=icon)
+window = sg.Window('Kelionės kalkuliatorius', 
+                layout, 
+                size=(1280, 480),
+                margins=(None, None), button_color=None, font='Italic 12 bold',
+                background_color='Dark Cyan', 
+                icon=icon,
+                alpha_channel=0.97, 
+                use_default_focus=True, 
+                grab_anywhere=True, 
+                resizable=True,
+                element_justification='left', 
+                titlebar_font='Italic 12 bold', titlebar_icon=icon)
 
-window2 = sg.Window('Keliones apskaita',
-                    layout2,
-                    element_justification='c',
-                    margins=(0, 0),
-                    element_padding=(0, 0),
+window2 = sg.Window('Kelionės apskaita', 
+                    layout2, 
+                    element_justification='c', 
+                    margins=(0,0), 
+                    element_padding=(0,0), 
                     finalize=True)
-
 
 window2['-T-'].expand(True, True, True)  # Make the Text element expand to take up all available space
 
