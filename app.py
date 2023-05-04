019450567b4fd659fba3e22d3679af3349f91ea0
Kobrius11 <kobrius11@gmail.com>
2023-05-04 14:26:37 +0300

    table_update

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import PySimpleGUI as sg
 from funkcijos import *
-from funkcijos import calculate_trip_info
 
 
 
+sudek_el_lst()
 # Layout
 layout = [
     [sg.Text('Keliones pavadinimas: ', background_color="Dark Cyan"), sg.Input(key='-NAME-', size=20)], 
@@ -15,10 +15,9 @@ layout = [
     [sg.Checkbox('Maistas', key='-FOOD_CHECK-', background_color="Dark Cyan")],
     [sg.Checkbox('Kelių mokestis', key='-TOLL_CHECK-', background_color="Dark Cyan")],
     [sg.Text('Valiutos tipas: ', background_color="Dark Cyan"), sg.Radio('Eurai', 'RADIO1', key='-EURO_RADIO-', default=True, background_color="Dark Cyan"), sg.Radio('Svarai', 'RADIO1', key='-POUND_RADIO-', background_color="Dark Cyan")],
-    [sg.Button('Skaičiuoti', button_color=('white', 'springgreen4'), use_ttk_buttons=True, focus=True), sg.Button('Išvalyti', use_ttk_buttons=True, focus=True), sg.Button('Rodyti ataskaitą', use_ttk_buttons=True, focus=True), sg.Button('Išeiti', button_color=('white', 'firebrick3'), use_ttk_buttons=True, focus=True)]
+    [sg.Button('Skaičiuoti', button_color=('white', 'springgreen4'), use_ttk_buttons=True, focus=True), sg.Button('Išvalyti', use_ttk_buttons=True, focus=True), sg.Button('Rodyti ataskaitą', use_ttk_buttons=True, focus=True), sg.Button('Išeiti', button_color=('white', 'firebrick3'), use_ttk_buttons=True, focus=True)],
+    [table]
     ]
-layout_table = [[table]]
-
 
 
 # Create the window
@@ -31,8 +30,7 @@ window = sg.Window('Kelionės kalkuliatorius', layout,
                 element_justification='left', 
                 titlebar_font='Italic 12 bold', titlebar_icon=icon)
 
-window_table = sg.Window('1', layout_table)
-
+# window_table = sg.Window('1', layout_table)
 
 
 CustomMeter()
@@ -40,38 +38,38 @@ CustomMeter()
 # Event loop
 while True:
     event, values = window.read()
-    
     if event in (sg.WINDOW_CLOSED, 'Išeiti'):
         break
         
     elif event == 'Skaičiuoti':
         try:
-             trip_info = calculate_trip_info(values)
-             keliones_pavadinimas, distance, speed, travel_time, fuel_capacity, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked, total_cost, fuel_consumption_total1 = trip_info
+            trip_info = calculate_trip_info(values)
+            data, keliones_pavadinimas, fuel_consumption_total1 = trip_info
+            #data, keliones_pavadinimas, distance, speed, travel_time, fuel_capacity, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked, total_cost, fuel_consumption_total1 = trip_info
+            
+            save_data(keliones_pavadinimas, data)
+            old_values = []
+            for value in lst:
+                old_values.append(value)
+            sudek_el_lst()
+            for value in lst:
+                if value in old_values:
+                    lst.remove(value)
+            window['-TABLE-'].update(values=lst)
         except Exception as e:
             sg.Popup('Something went wrong', e)
             continue
         else:
-            sg.Popup(f"{total_cost}, {travel_time}, {fuel_consumption_total1}, kuro bako talpa: {fuel_capacity}")
+            sg.Popup(f"Visos islaidos: {data['total_cost']},\n Visas laikas: {data['travel_time']},\n Kuro sanaudos: {fuel_consumption_total1},\n kuro bako talpa: {data['fuel_capacity']}")
         
-        data = {
-                "distance": distance,
-                "speed": speed,
-                "travel time": travel_time,
-                "fuel capacity": fuel_capacity, 
-                "fuel_comsumption": fuel_consumption,
-                "total cost": total_cost,
-        }
-        save_data(keliones_pavadinimas, data)
+        # save_data(keliones_pavadinimas, data)
             
     elif event == 'Rodyti ataskaitą':
-        # sg.Popup(f"{data}")
-        sudek_el_lst()
+        pass
 
-        event = window_table.read()
-        if event == sg.WIN_CLOSED:
-            window_table.close
-            pass
+    elif event == 'Istrinti kelione':
+        pass
+
                 
     elif event == 'Išvalyti':
         for key in ['-NAME-', '-DISTANCE-', '-SPEED-', '-FUEL_CAPACITY-', '-FUEL_PRICE-', '-FUEL_CONSUMPTION-']:
