2ae0c2f213dca18ab63d6f28e3e8d5ce20cb257e
Kobrius11 <kobrius11@gmail.com>
2023-05-04 11:52:11 +0300

    pakomitinau

--- funkcijos.py
+++ funkcijos.py
@@ -3,8 +3,20 @@ from datetime import date
 import PySimpleGUI as sg
 
 siandien = date.today().strftime("%d-%m-%Y")
-
+filename = 'code_data.json'
 icon = b'iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAFZklEQVR4nO3TWWxUVRwG8O/uc2fuLHfaKVO6sJRpS9kKCaCCpfKgDSYgEhAe5IFggEBCxKAPJmhifFDiEhMxmAiGKE0KYtkiLpVVC1ikmEKhWKaddtrZp7PfO3fuPT6ZGFMYEHzjez7n/8v3zznA4zxkurq6uLa2NqbYOfpRwz3e2MrBUKjyf4WPXiHT9nUSDwD0EMJvPtBdZhikRWKECcXuUg8CEUKoQ4dAr1lD6XsGiez9dWij79bNpYGRQFJN6rPURLB0xiTW/uRc8aWtm7a232sW+yDwmYEBYd3aKcrO44qn/aNf2hP+voZE2ItEMAJGV8ExLGJSFUIjqYnFZj3Qqt/ZdqTkuc0dTT3nez7PJvQGomlIBOPIp8MwOECnbdBZe2L58qaTxWbdd+Ol6z+rcNU3HxRFqSmvqpjvimCoK4asJCMGAoMRoGo88pSUnTpvaeKRwEveOs1Wy+59DjPVpIe6MTLqRerCEbzIOkBbZ6IDTuS1JAxKRcjvFbZs3y4Um3lfqz7zdrOuZ0ePnv5iGw5+shlnvvkAt/N5vMvkICl9eAIJOCQ7CnoODqfMrlj1Cv9IGgPgqhgy+zotYUF1IyizBSboGE5H4OEkaLoGJ0sjKdpBURTNWMxFf0tR+PUDt596df/ge4oqLG6c1Qw6mUTs0jHIsg01mgICDXYtAX4si4RjBnjRQu6nyT3h9suJuhOXA8f7e286x66cRq1FgdlIob5ERlkmBcFUBo/ZjE5UYYhQoEgBPG8CJwhF8bvCbRdHpwz5BltrZcUp1MgY7HViphBBlMrjiuFEuTgZDsrAgGBBi5HGMakOejYPo6BBfJjGF08dWtDbNzaX0kZRUdeCaj0OcWwAE/NxZLgJqOYUiDSFQC6BOxqPWfQIKiQ3AnYbcrncf4cjkaAjm9FgM7Pw9Q8gMHQDfyh+iI5yeKwaJqgZmDgBQ9IkDCpWeKwWKKwJajZDcwxT9LeMC7cFiXR231cb9Du/g+VYgJhhnToPhG/E9OwohPBVXFSACCVAEjX0spW4UcjBEDTIui3w/Lwp/i1vfvqsPxqpX7R91/43plOpfxvjPvvXdn+/MDR89aKvPwC3W0BlzVxkwsNIKRyun/0O3ddPweysgXtKHWrqaxEZiUJNByCXuCBI5SOBaObl7FDnzplV81vslrJrllmNe8Nq2jJ8Z/jmb+27Tty18fR5jbWVk2V4akMQeAZ1DbPR8ZMXP5/8EVDCaFqxAdcu/IAJNjPWLm9GnzeIwxf8CHIiBN46seCiOkrKajEjEsa1vnNzjArsEfkqRPvPh9ft2NvQ+uGmyLgwC2ZSy7L54HQABChoBO2tZ0HnvJDdpWheuRFBby/cLieqnDyycWD9CwvhqvYgHo1CVTVAEOA7cgxGRsBYyIesqqBcLjgHrhxzAxgf7jj8rUii5eCgwCSaEAoE0XP5FLRcCC67DTPKCRwOG7xDOjq7Y/D7o7h06RwEQQBL5cCyNMwWE4yICloug0mSEYmnUDm5Fn9e7aLuuuquC50h/x0NqWQKgACKqAgH0wZN03Q44Ec66EM8Fk7HYvTw11+2mlLxCKcaToHQdGn1VAk8xUBJ5pHOjMHK0Ih6Y/D130Kp82mSzWaN8UwAQF3dTmv9zMVLRNHaT1ECYVmpw+Va0lg5ccEGm10m8xc9Q0rLyt9fvbqB3717t8XpnGYTgBqapmIACMMwgwJvIqIokbLKaaTEPUkRzWZS4WmMW63Wkru+6r/jcDiWZDKJZQBp1TR0A4As23ckk+lKm832cTwe9/3zvCRJq1RVncPzOGwy2ZYBhljIF3iaxXmG4epy6ZQ/o2ht9zIf53EeOn8BWy1K8ndljBwAAAAASUVORK5CYII='
+lst = []
+
+table = sg.Table(
+    values= lst,
+    headings=['Keliones pavadinimas', "distance", "speed", "travel time", "fuel capacity", "fuel_comsumption", "total cost"], 
+    auto_size_columns=True,
+    display_row_numbers=False,
+    justification='center', key='-TABLE-',
+    selected_row_colors='red on yellow',
+    enable_events=True,
+    expand_x=True,
+    expand_y=True)
 
 def calculate_total_cost(distance, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked):
     fuel_cost = distance * fuel_consumption * fuel_price / 100
@@ -52,7 +64,7 @@ def load_data(filename):
 def CustomMeter():
     # layout the form
     layout = [[sg.Text('A custom progress meter')],
-              [sg.ProgressBar(5000, orientation='h',
+              [sg.ProgressBar(50, orientation='h',
                               size=(20, 20), key='progress')],
               [sg.Cancel()]]
 
@@ -60,7 +72,7 @@ def CustomMeter():
     window = sg.Window('Custom Progress Meter', layout)
     progress_bar = window['progress']
     # loop that would normally do something useful
-    for i in range(5000):
+    for i in range(50):
         # check to see if the cancel button was clicked and exit loop if clicked
         event, values = window.read(timeout=0, timeout_key='timeout')
         if event == 'Išeiti' or event == None:
@@ -70,3 +82,12 @@ def CustomMeter():
     # done with loop... need to destroy the window as it's still open
     window.CloseNonBlocking()
 
+def sudek_el_lst():
+    data = load_data(filename)
+    for name, dict in data.items():
+        row = []
+        row.append(name)
+        for val in dict.values():
+            row.append(val)
+        lst.append(row)
+          
