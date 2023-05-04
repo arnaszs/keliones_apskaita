b7efed438dae223cc9bd63f8b7bd72fd3afe5d5f
Kobrius11 <kobrius11@gmail.com>
2023-05-04 08:37:51 +0300

    pakomitinu

--- app.py
+++ app.py
@@ -16,21 +16,17 @@ layout = [
     [sg.Checkbox('Maistas', key='-FOOD_CHECK-', background_color="Dark Cyan")],
     [sg.Checkbox('Kelių mokestis', key='-TOLL_CHECK-', background_color="Dark Cyan")],
     [sg.Text('Valiutos tipas: ', background_color="Dark Cyan"), sg.Radio('Eurai', 'RADIO1', key='-EURO_RADIO-', default=True, background_color="Dark Cyan"), sg.Radio('Svarai', 'RADIO1', key='-POUND_RADIO-', background_color="Dark Cyan")],
-    [sg.Button('Skaičiuoti', use_ttk_buttons=True, focus=True), sg.Button('Išvalyti', use_ttk_buttons=True, focus=True), sg.Button('Rodyti ataskaitą', use_ttk_buttons=True, focus=True), sg.Button('Išeiti', use_ttk_buttons=True, focus=True)]]
+    [sg.Button('Skaičiuoti', button_color=('white', 'springgreen4'), use_ttk_buttons=True, focus=True), sg.Button('Išvalyti', use_ttk_buttons=True, focus=True), sg.Button('Rodyti ataskaitą', use_ttk_buttons=True, focus=True), sg.Button('Išeiti', button_color=('white', 'firebrick3'), use_ttk_buttons=True, focus=True)]]
 
 # Create the window
 window = sg.Window('Kelionės kalkuliatorius', layout, 
                  size=(640, 480),
                  element_padding=None, margins=(None, None), button_color=None, font='Italic 12 bold',
-                 background_color='Dark Cyan', border_depth=None,
+                 background_color='Dark Cyan', 
                  icon=icon,
-                 alpha_channel=0.97, use_default_focus=True, text_justification=None,
-                 no_titlebar=False, grab_anywhere=True, grab_anywhere_using_control=False, keep_on_top=None, resizable=True, 
-                 disable_minimize=False, right_click_menu=None, transparent_color=None, debugger_enabled=True,
+                 alpha_channel=0.97, use_default_focus=True, grab_anywhere=True, resizable=True,
                  element_justification='left', 
-                 titlebar_font='Italic 12 bold', titlebar_icon=icon,
-                 scaling=None,
-                 metadata=None)
+                 titlebar_font='Italic 12 bold', titlebar_icon=icon)
 
 # Event loop
 while True:
@@ -38,21 +34,26 @@ while True:
     if event in (sg.WINDOW_CLOSED, 'Išeiti'):
         break
     elif event == 'Skaičiuoti':
+        try:
         # Do the calculations here
-        keliones_pavadinimas = values['-NAME-'] + str(siandien)
-        distance = float(values['-DISTANCE-'])
-        speed = float(values['-SPEED-'])
-        travel_time = calculate_travel_time(distance, speed)
-        fuel_capacity = values['-FUEL_CAPACITY-']
-        fuel_consumption = float(values['-FUEL_CONSUMPTION-'])
-        fuel_price = float(values['-FUEL_PRICE-'])
-        food_checked = values['-FOOD_CHECK-']
-        toll_checked = values['-TOLL_CHECK-']
-        euro_checked = values['-EURO_RADIO-']
-        pound_checked = values['-POUND_RADIO-']
-        total_cost = calculate_total_cost(distance, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked)
-        fuel_consumption_total1 = fuel_consumption_total(distance, fuel_consumption)
-        sg.Popup(f"{total_cost}, {travel_time}, {fuel_consumption_total1}, kuro bako talpa: {fuel_capacity}")
+            keliones_pavadinimas = values['-NAME-'] + str(siandien)
+            distance = float(values['-DISTANCE-'])
+            speed = float(values['-SPEED-'])
+            travel_time = calculate_travel_time(distance, speed)
+            fuel_capacity = values['-FUEL_CAPACITY-']
+            fuel_consumption = float(values['-FUEL_CONSUMPTION-'])
+            fuel_price = float(values['-FUEL_PRICE-'])
+            food_checked = values['-FOOD_CHECK-']
+            toll_checked = values['-TOLL_CHECK-']
+            euro_checked = values['-EURO_RADIO-']
+            pound_checked = values['-POUND_RADIO-']
+            total_cost = calculate_total_cost(distance, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked)
+            fuel_consumption_total1 = fuel_consumption_total(distance, fuel_consumption)
+        except Exception as e:
+            sg.Popup('Something went wrong', e)
+            continue
+        else:
+            sg.Popup(f"{total_cost}, {travel_time}, {fuel_consumption_total1}, kuro bako talpa: {fuel_capacity}")
         
         data = {
         keliones_pavadinimas: {
@@ -82,8 +83,8 @@ while True:
             sg.Popup(f"{data}")
             data = data
 
-
     elif event == 'Išvalyti':
+        window['-NAME-'].update('')
         window['-DISTANCE-'].update('')
         window['-FUEL_CONSUMPTION-'].update('')
         window['-FUEL_CAPACITY-'].update('')
---
+++ travel_tracker.py
@@ -0,0 +1,76 @@
+import PySimpleGUI as sg
+import json
+import os
+
+def calculate_costs(distance, fuel_price, tank_capacity, fuel_consumption, food_budget=0):
+    fuel_cost = (distance / 100) * fuel_consumption * fuel_price
+    refuels = distance // (tank_capacity * 100)
+    if distance % (tank_capacity * 100) != 0:
+        refuels += 1
+    food_cost = food_budget
+    total_cost = fuel_cost * refuels + food_cost
+    return total_cost
+
+def save_data(data):
+    with open('travel_data.json', 'w') as f:
+        json.dump(data, f)
+
+def load_data():
+    if os.path.isfile('travel_data.json'):
+        with open('travel_data.json', 'r') as f:
+            return json.load(f)
+    else:
+        return {}
+
+def main():
+    layout = [
+        [sg.Text('How far is the distance to your final destination (km)?*'), sg.InputText(key='distance')],
+        [sg.Text('What is the fuel price in the area?*'), sg.InputText(key='fuel_price')],
+        [sg.Text('What is your fuel tank capacity?*'), sg.InputText(key='tank_capacity')],
+        [sg.Text('What is your fuel consumption?*'), sg.InputText(key='fuel_consumption')],
+        [sg.Text('How much money do you prefer to spend on food?(optional)'), sg.InputText(key='food_budget')],
+        [sg.Button('Calculate', button_color=('white', 'springgreen4')), sg.Button('Save'), sg.Button('Show'), sg.Button('Total Costs'), sg.Button('Exit', button_color=('white', 'firebrick3'))],
+        [sg.Output(size=(80, 20))]
+    ]
+
+    window = sg.Window('Europe Travel Tracker', layout)
+
+    data = load_data()
+
+    while True:
+        event, values = window.read()
+        if event == sg.WINDOW_CLOSED or event == 'Exit':
+            break
+        elif event == 'Calculate':
+            try:
+                distance = float(values['distance'])
+                fuel_price = float(values['fuel_price'])
+                tank_capacity = float(values['tank_capacity'])
+                fuel_consumption = float(values['fuel_consumption'])
+                food_budget = float(values['food_budget']) if values['food_budget'] else 0
+                total_cost = calculate_costs(distance, fuel_price, tank_capacity, fuel_consumption, food_budget)
+                print(f'Total cost of the trip: {total_cost:.2f} euros')
+                data['distance'] = distance
+                data['fuel_price'] = fuel_price
+                data['tank_capacity'] = tank_capacity
+                data['fuel_consumption'] = fuel_consumption
+                data['food_budget'] = food_budget
+                data['total_cost'] = total_cost
+            except ValueError:
+                print('Please enter valid numbers')
+        elif event == 'Save':
+            save_data(data)
+            print('Data saved successfully')
+        elif event == 'Show':
+            for key, value in data.items():
+                print(f'{key}: {value}')
+        elif event == 'Total Costs':
+            if 'total_cost' in data:
+                print(f'Total cost of the trip: {data["total_cost"]:.2f} euros')
+            else:
+                print('Please calculate the total cost first')
+    
+    window.close()
+
+if __name__ == '__main__':
+    main()
