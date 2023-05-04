ed198bc2510ca4b1c1378d2dcabf14dfc51d31d4
RenaldasZ <spikeris2@gmail.com>
2023-05-04 15:16:02 +0300

    last comit

--- app.py
+++ app.py
@@ -58,6 +58,7 @@ while True:
         break
         
     elif event == 'Skaičiuoti':
+        print(values)
         try:
             data, keliones_pavadinimas, fuel_consumption_total1 = calculate_trip_info(values)
             window['-TABLE-'].update(values=update_table(keliones_pavadinimas, data))
@@ -80,42 +81,4 @@ while True:
             window[key].update(False)
         window['-EURO_RADIO-'].update(True)
 
-    # Event loop
-    while True:
-        event, values = window.read()
-        
-        if event in (sg.WINDOW_CLOSED, 'Išeiti'):
-            break
-            
-        elif event == 'Skaičiuoti':
-            try:
-                trip_info = calculate_trip_info(values)
-                keliones_pavadinimas, distance, speed, travel_time, fuel_capacity, fuel_consumption, fuel_price, food_checked, toll_checked, euro_checked, pound_checked, total_cost, fuel_consumption_total1 = trip_info
-            except Exception as e:
-                sg.Popup('Something went wrong', e)
-                continue
-            else:
-                sg.Popup(f"{total_cost}, {travel_time}, {fuel_consumption_total1}, kuro bako talpa: {fuel_capacity}")
-            
-            data = {
-                    "distance": distance,
-                    "speed": speed,
-                    "travel time": travel_time,
-                    "fuel capacity": fuel_capacity, 
-                    "fuel_comsumption": fuel_consumption,
-                    "total cost": total_cost,
-            }
-            save_data(keliones_pavadinimas, data)
-                
-        elif event == 'Rodyti ataskaitą':
-            # sg.Popup(f"{data}")
-            sudek_el_lst()
-                            
-        elif event == 'Išvalyti':
-            for key in ['-NAME-', '-DISTANCE-', '-SPEED-', '-FUEL_CAPACITY-', '-FUEL_PRICE-', '-FUEL_CONSUMPTION-']:
-                window[key].update('')
-            window['-FOOD_CHECK-'].update(False)
-            window['-TOLL_CHECK-'].update(False)
-
-    # Close the window
-    window.close()
+window.close()
